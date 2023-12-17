3fccb99edf5d5c72adba95c77ce033fe08771697
Kaio Masaru Nakazono <kmn1405.cg@gmail.com>
2023-12-17 10:38:02 -0300

    add validação cpf no serializer

--- setup/urls.py
+++ setup/urls.py
@@ -5,7 +5,8 @@ from rest_framework import routers
 from clientes.views import ClienteViewSet
 
 router = routers.DefaultRouter()
-router.register('clientes', ClienteViewSet)
+router.register('clientes', ClienteViewSet, basename='Clientes')
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
